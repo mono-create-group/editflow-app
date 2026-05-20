fac943e69c2a80e1f7055eec0043e35b7d3b2252
mono-create-group <mono.create.group@gmail.com>
2026-05-20 10:38:10 +0900

    Update sw.js cache version to editflow-20260520-3

--- sw.js
+++ sw.js
@@ -1,43 +1,12 @@
-// EditFlow Service Worker v20260520-2
-const CACHE = 'editflow-20260520-2';
-const BASE = '/editflow-app';
-
+// EditFlow Service Worker v20260520-3
+const CACHE = 'editflow-20260520-3';
+const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE).then(c =>
-      c.add(BASE + '/index.html').catch(()=>{})
-    )
-  );
-  self.skipWaiting();
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
 });
-
 self.addEventListener('activate', e => {
-  e.waitUntil(
-    caches.keys()
-      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
-      .then(() => self.clients.claim())
-  );
+  e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
 self.addEventListener('fetch', e => {
-  const url = new URL(e.request.url);
-  const isPage = e.request.mode === 'navigate' ||
-    url.pathname.endsWith('.html') || url.pathname === BASE + '/';
-
-  if (isPage) {
-    // HTMLは常にネットワーク優先 → 最新版を確実に取得
-    e.respondWith(
-      fetch(e.request, {cache: 'no-store'})
-        .then(res => {
-          const clone = res.clone();
-          caches.open(CACHE).then(c => c.put(e.request, clone));
-          return res;
-        })
-        .catch(() => caches.match(e.request))
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(r => r || fetch(e.request))
-    );
-  }
+  e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
