34f6aa22bd8e69cb599555f8bfe79cbe9dd0ba19
mono-create-group <mono.create.group@gmail.com>
2026-05-20 21:14:14 +0900

    sw: v20260520-4

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260520-3
-const CACHE = 'editflow-20260520-3';
+// EditFlow Service Worker v20260520-4
+const CACHE = 'editflow-20260520-4';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
