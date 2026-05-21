f4865646d1be2e4e838b0413d04ae3db8c4da11b
mono-create-group <mono.create.group@gmail.com>
2026-05-21 23:24:15 +0900

    deploy v20260521-2: remove RPG, add gym rest day

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260521-1
-const CACHE = 'editflow-20260521-1';
+// EditFlow Service Worker v20260521-2
+const CACHE = 'editflow-20260521-2';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
