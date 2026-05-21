6bc2b7baac98fd4c7bcd09393a30f21e053edd15
mono-create-group <mono.create.group@gmail.com>
2026-05-21 23:16:30 +0900

    deploy v20260521-1: tomorrow tab, task sort, realtime sync

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260520-7
-const CACHE = 'editflow-20260520-7';
+// EditFlow Service Worker v20260521-1
+const CACHE = 'editflow-20260521-1';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
