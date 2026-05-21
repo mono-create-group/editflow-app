5316d912fef301fb8eea0c1483cd7544f32fe3df
mono-create-group <mono.create.group@gmail.com>
2026-05-22 00:12:14 +0900

    deploy v20260522-3: fixed schedule at top of today view

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260522-2
-const CACHE = 'editflow-20260522-2';
+// EditFlow Service Worker v20260522-3
+const CACHE = 'editflow-20260522-3';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
