c0818f118ec7fe78e3a0f6ca6e5fa5817bfb47ac
EditFlow Deploy <deploy@editflow>
2026-05-22 23:25:58 +0900

    v20260522-11: 明日のタスク並び替え対応・順番を今日に引き継ぎ

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260522-10
-const CACHE = 'editflow-20260522-10';
+// EditFlow Service Worker v20260522-11
+const CACHE = 'editflow-20260522-11';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
