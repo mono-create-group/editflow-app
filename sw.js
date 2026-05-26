8832ce2bb8461b43451facf79e0ec105afe67c9b
EditFlow Deploy <deploy@editflow>
2026-05-26 13:00:23 +0900

    feat: add 入金済みボタン to job cards, profit pending section, payment status (v20260526-24)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260523-23
-const CACHE = 'editflow-20260523-23';
+// EditFlow Service Worker v20260526-24
+const CACHE = 'editflow-20260526-24';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
