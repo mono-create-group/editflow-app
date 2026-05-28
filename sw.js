c55f1eaf195acf509eccf92aa7505995f2705e8f
山本夏鈴 <karin@yamamotonatsusuzunoMacBook-Air.local>
2026-05-28 12:26:46 +0900

    feat: add 採用予定ワーカー section with promote flow (v20260526-26)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260526-25
-const CACHE = 'editflow-20260526-25';
+// EditFlow Service Worker v20260526-26
+const CACHE = 'editflow-20260526-26';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
