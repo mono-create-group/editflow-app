613a6b1782a932d2873e425dad0c6006a6abd92b
山本夏鈴 <karin@yamamotonatsusuzunoMacBook-Air.local>
2026-05-31 16:39:53 +0900

    fix: journal calendar month navigation + show all days incl 31st (v20260529-28)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260529-27
-const CACHE = 'editflow-20260529-27';
+// EditFlow Service Worker v20260529-28
+const CACHE = 'editflow-20260529-28';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
