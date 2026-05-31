f61b7713f4092c9a899b5376d05654102085ec44
山本夏鈴 <karin@yamamotonatsusuzunoMacBook-Air.local>
2026-05-31 16:42:06 +0900

    fix: time schedule lock checks task startTime in schedule tab (v20260529-29)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260529-28
-const CACHE = 'editflow-20260529-28';
+// EditFlow Service Worker v20260529-29
+const CACHE = 'editflow-20260529-29';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
