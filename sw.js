4553e124be16d9e754cc29c11f00f3df7f1f7d1b
山本夏鈴 <karin@yamamotonatsusuzunoMacBook-Air.local>
2026-06-01 19:50:38 +0900

    feat: 初稿・クライアント提出確認ロック + 全ページバナー (v20260601-33)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // EditFlow Service Worker v20260529-31
-const CACHE = 'editflow-20260601-32';
+const CACHE = 'editflow-20260601-33';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
