df73561d01dcdce2e3db9f40da0e9e4e19f6d20c
山本夏鈴 <karin@yamamotonatsusuzunoMacBook-Air.local>
2026-06-02 11:17:41 +0900

    fix: 目標・KPIページに事業部削除ボタン追加 (v20260602-38)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // EditFlow Service Worker v20260529-31
-const CACHE = 'editflow-20260602-37';
+const CACHE = 'editflow-20260602-38';
 const URLS = ['./', './editflow.html'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(URLS)).then(() => self.skipWaiting()));
