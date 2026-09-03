f92b22bee1ee7be47becc33a8106b590ef729092
中村航汰 <nakamurawataru@MacBook-Pro-2.local>
2026-09-03 15:31:04 +0900

    Match monthly calendar typography to internal app

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
-// EditFlow Service Worker v20260903-05
-const CACHE = 'editflow-20260903-05';
-const LATEST_APP_URL = new URL('./editflow.html?app=20260903-05', self.registration.scope).href;
+// EditFlow Service Worker v20260903-06
+const CACHE = 'editflow-20260903-06';
+const LATEST_APP_URL = new URL('./editflow.html?app=20260903-06', self.registration.scope).href;
 const APP_SHELL_URL = new URL('./editflow.html', self.registration.scope).href;
 const URLS = ['./', './editflow.html', './ai-bridge-client.js'];
 self.addEventListener('install', e => {
