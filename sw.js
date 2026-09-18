8c0f057551dd40c38678b057e0f5450569a469b7
中村航汰 <nakamurawataru@MacBook-Pro-2.local>
2026-09-18 18:47:37 +0900

    同一内容の同期ループを停止

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-// EditFlow Service Worker v20260918-08
-const CACHE = 'editflow-20260918-08';
+// EditFlow Service Worker v20260918-09
+const CACHE = 'editflow-20260918-09';
 const APP_SHELL_URL = new URL('./editflow.html', self.registration.scope).href;
 const URLS = ['./', './editflow.html', './ai-bridge-client.js'];
 self.addEventListener('install', e => {
